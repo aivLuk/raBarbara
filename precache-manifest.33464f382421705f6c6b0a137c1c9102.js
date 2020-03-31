4e053e1be855e255b2b1a80e4b54c2d082c321cc
Aivaras <lukauskasaivaras@gmail.com>
2020-03-31 14:38:15 +0300

    Updates

---
+++ precache-manifest.33464f382421705f6c6b0a137c1c9102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c5721b1a7e90370496817e3212a11b",
+    "url": "/raBarbara/index.html"
+  },
+  {
+    "revision": "b3726977e73527fe185e",
+    "url": "/raBarbara/static/css/main.f444bc27.chunk.css"
+  },
+  {
+    "revision": "48c05a380f8d80b54941",
+    "url": "/raBarbara/static/js/2.3eecd268.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/raBarbara/static/js/2.3eecd268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3726977e73527fe185e",
+    "url": "/raBarbara/static/js/main.64b27417.chunk.js"
+  },
+  {
+    "revision": "31d0dbe139d0cf1e524b",
+    "url": "/raBarbara/static/js/runtime-main.69cc3b05.js"
+  },
+  {
+    "revision": "73c1c773b48f78c48696c513d431c296",
+    "url": "/raBarbara/static/media/banner.73c1c773.jpg"
+  }
+]);
